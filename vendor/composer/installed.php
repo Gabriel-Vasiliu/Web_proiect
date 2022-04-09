37a21e3a63b6bde0e0af614eee5e9a4a08a3a0c2
stoleriudaniel47 <stoleriu.daniel47@gmail.com>
2022-04-09 17:31:27 +0300

    Add composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'c0cd7741d4049de07e0e96bee74a63de8c0febee',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'c0cd7741d4049de07e0e96bee74a63de8c0febee',
+            'dev_requirement' => false,
+        ),
+    ),
+);
