cca3c9280e02b7667eb6558bd3a7bf5fec36ee04
stoleriudaniel47 <stoleriu.daniel47@gmail.com>
2022-04-09 17:29:28 +0300

    Update Router

--- core/Router.php
+++ core/Router.php
@@ -1,22 +1,60 @@
 <?php
 
-class Router{
-    protected $routes = [];
+class Router
+{
+    protected $routes = [
+        'GET' => [],
+        'POST' => []
+    ];
 
-    public function define($routes){
+    public function define($routes)
+    {
         $this->routes = $routes;
     }
 
-    public static function load($file){
+    public function get($uri, $controller)
+    {
+        $this->routes['GET'][$uri] = $controller;
+    }
+
+    public function post($uri, $controller)
+    {
+        $this->routes['POST'][$uri] = $controller;
+    }
+
+    public static function load($file)
+    {
         $router = new static;
         require $file;
         return $router;
     }
 
-    public function direct($uri){
-        if(array_key_exists($uri, $this->routes)){
-            return $this->routes[$uri];
+    public function direct($uri, $requestType)
+    {
+        if (array_key_exists($uri, $this->routes[$requestType])) {
+            if (is_array($this->routes[$requestType][$uri])) {
+                return $this->callAction($this->routes[$requestType][$uri]);
+            } else {
+                $data = explode('@', $this->routes[$requestType][$uri]);
+                $data[0] = "App\\Controllers\\{$data[0]}";
+                return $this->callAction($data);
+            }
         }
         throw new Exception('Nu este nicio ruta definita pentru acest URI!');
+    }
+
+    protected function callAction($data)
+    {
+        [$controller, $action] = $data;
+        
+        $controller = new $controller;
+        
+        if (!method_exists($controller, $action)) {
+            throw new Exception(
+                "{$controller} does not respond to the action {$action} action"
+            );
+        }
+
+        return $controller->$action();
     }
 }
