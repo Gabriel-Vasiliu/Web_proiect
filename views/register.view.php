5ea5dbaaeddbeb710698010e6ebd97e09373a42f
stoleriudaniel47 <stoleriu.daniel47@gmail.com>
2022-04-10 19:27:00 +0300

    Create form in register

--- views/register.view.php
+++ views/register.view.php
@@ -1,5 +1,10 @@
 <?php require 'partials/header.php'; ?>
 
-<h1> Register page. </h1>
+<?php $form = \App\Core\Form\Form::begin('', 'post'); ?>
+    <?php echo $form->field($model, 'username'); ?>
+    <?php echo $form->field($model, 'password')->passwordField(); ?>
+    <?php echo $form->field($model, 'confirmPassword')->passwordField(); ?>
+    <button type="submit" class="btn btn-primary">Submit</button>
+<?php echo \App\Core\Form\Form::end(); ?>
 
 <?php require 'partials/footer.php'; ?>
