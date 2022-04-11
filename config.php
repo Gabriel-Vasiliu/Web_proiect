0a38f46fbd550db2ba0631fbd2a26c85b857e93b
stoleriudaniel47 <stoleriu.daniel47@gmail.com>
2022-04-11 15:18:20 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'database' => [
+        'name' => 'phpdb',
+        'port' => '3308',
+        'username' => 'root',
+        'password' => 'pkk990',
+        'connection' => '127.0.0.1',
+        'options' => [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
+        ]
+    ]
+];
