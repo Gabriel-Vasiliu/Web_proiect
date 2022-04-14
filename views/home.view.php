7f11b26bacad0b5e26ff6471e7515b1bd3f747fd
Gabriel-Vasiliu <79076883+Gabriel-Vasiliu@users.noreply.github.com>
2022-04-14 16:19:18 +0300

    home page

--- views/home.view.php
+++ views/home.view.php
@@ -1,5 +1,12 @@
 <?php require 'partials/header.php'; ?>
-<h1>Home page.</h1>
+
+<p class="descriere"> 
+    Colectezi recipiente? Ai nevoie de un site in care sa iti gestionezi colectia ta favorita? Noi avem solutia!<br>
+    <b>Collecting Bottles</b> este site-ul perfect pentru tine! <br> Acum poti sa iti editezi colectia 
+    dupa bunul plac si sa o imparti cu ceilalti!
+</p>
+
+<img class="home-gif" src="./images/sticla1.gif" alt="sticla">
 
 <?php if(App\Core\App::get('session')->getFlash('success')): ?>
     <div class="alert alert-success">
