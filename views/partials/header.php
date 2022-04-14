4834a6bf9fb5beda37cf4c4a3582c6ba875bd935
stoleriudaniel47 <stoleriu.daniel47@gmail.com>
2022-04-14 20:23:00 +0300

    Fix header and footer

--- views/partials/header.php
+++ views/partials/header.php
@@ -5,25 +5,23 @@
     <meta charset="utf-8">
     <title>Collecting Bottles</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <meta name="author" content="Stoleriu Daniel, Vasiliu Gabriel">
+    <link rel="stylesheet" href="/style.css">
     <link rel="icon" type="image/x-icon" href="/images/favicon1.ico">
   </head>
   <body>
+  <header>
     <nav>
-      <input type="checkbox" id="check">
-      <label for="check" class="checkbtn">
-        <i class="fas fa-bars"></i>
-      </label>
-      <label class="logo"></label>
+    <div id="nav-menu-thumb"></div>
+        <input id="nav-menu-toggle" type="checkbox">
             <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/top">Top</a></li>
-                <li><a href="/search">Search</a></li>
+                <li><a class="nav-link" href="/">Home</a></li>
+                <li><a class="nav-link" href="/top">Top</a></li>
+                <li><a class="nav-link" href="/search">Search</a></li>
                 
                 <?php if (App\Core\App::isGuest()): ?>
-                <li><a href="/login">Login</a></li>
-                <li><a href="/register">Register</a></li>
+                <li><a class="nav-link" href="/login">Login</a></li>
+                <li><a class="nav-link" href="/register">Register</a></li>
                 <?php else: ?>
                 <li>
                   <a href="/logout">Welcome, <?php echo App\Core\App::$user->getDisplayName(); ?>
@@ -33,6 +31,7 @@
                 <?php endif; ?>
             </ul>
         </nav>
+  </header>
   <main>
 
         
